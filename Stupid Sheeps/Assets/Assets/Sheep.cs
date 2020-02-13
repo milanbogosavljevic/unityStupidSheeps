6c21ac87e9caad4cc7de7b0cb43f43c6dd457fd6
milanbogosavljevic <andrej792885@gmail.com>
2020-02-13 21:11:05 +0100

    additional files

---
+++ Stupid Sheeps/Assets/Assets/Sheep.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    float speed;
+    Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+
+        if(GetComponent<SpriteRenderer>().flipX == true)
+        {
+            speed = -1.0f;
+        }
+        else
+        {
+            speed = 1.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x += speed * Time.deltaTime;
+        transform.position = position;
+
+        if(transform.position.x > 6)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            speed = -1.0f;
+        }
+
+        if (transform.position.x < -6)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            speed = 1.0f;
+        }
+    }
+}
