6983385614853eea1baf386530d24285e879db8c
milanbogosavljevic <andrej792885@gmail.com>
2020-02-17 22:02:29 +0100

    new logic added

---
+++ Stupid Sheeps/Assets/Assets/Saw.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+
+    //private Rigidbody2D myBody;
+    [SerializeField] private float positionLeft;
+    [SerializeField] private float positionRight;
+    [SerializeField] private float positionY;
+
+    private float rotationSpeed = 550f;
+
+    
+    void Start()
+    {
+        //myBody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+
+    public void MoveSaw()
+    {
+        if (transform.position.x == positionLeft)
+        {
+            transform.position = new Vector2(positionRight, positionY);        
+        }
+        else
+        {
+            transform.position = new Vector2(positionLeft, positionY);
+        }
+    }
+}
--- Stupid Sheeps/Assets/Assets/Sheep.cs
+++ Stupid Sheeps/Assets/Assets/Sheep.cs
@@ -4,40 +4,81 @@ using UnityEngine;
 
 public class Sheep : MonoBehaviour
 {
-    float speed;
-    Vector3 position;
+    /*    float speed;
+        Vector3 position;*/
 
-    // Start is called before the first frame update
+    private Rigidbody2D myBody;
+
+    [SerializeField] private float speed;
+    [SerializeField] private bool startToLeft;
+
     void Start()
     {
-        position = transform.position;
+        /*        position = transform.position;
 
-        if(GetComponent<SpriteRenderer>().flipX == true)
+                if(GetComponent<SpriteRenderer>().flipX == true)
+                {
+                    speed = -1.0f;
+                }
+                else
+                {
+                    speed = 1.0f;
+                }*/
+
+        myBody = GetComponent<Rigidbody2D>();
+        //if (GetComponent<SpriteRenderer>().flipX == true)
+        if (startToLeft == true)
         {
-            speed = -1.0f;
+            speed = -0.6f;
         }
         else
         {
-            speed = 1.0f;
+            speed = 0.6f;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        position.x += speed * Time.deltaTime;
-        transform.position = position;
+        /*        position.x += speed * Time.deltaTime;
+                transform.position = position;
 
-        if(transform.position.x > 6)
+                if(transform.position.x > 6)
+                {
+                    GetComponent<SpriteRenderer>().flipX = true;
+                    speed = -1.0f;
+                }
+
+                if (transform.position.x < -6)
+                {
+                    GetComponent<SpriteRenderer>().flipX = false;
+                    speed = 1.0f;
+                }*/
+
+        
+        if (transform.position.x >= 6.0f)
         {
-            GetComponent<SpriteRenderer>().flipX = true;
-            speed = -1.0f;
+            //GetComponent<SpriteRenderer>().flipX = true;
+            Quaternion rot = Quaternion.Euler(0f, -180f, 0f);
+            this.transform.rotation = rot;
+            speed = -0.6f;
         }
 
-        if (transform.position.x < -6)
+        if (transform.position.x <= -6.0f)
         {
-            GetComponent<SpriteRenderer>().flipX = false;
-            speed = 1.0f;
+            //GetComponent<SpriteRenderer>().flipX = false;
+            Quaternion rot = Quaternion.Euler(0f, 0f, 0f);
+            this.transform.rotation = rot;
+            speed = 0.6f;
+        }
+        myBody.velocity = new Vector2(speed, myBody.velocity.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Saw")
+        {
+            Debug.Log("Destroy Object");
         }
     }
 }
